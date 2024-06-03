201a817851d6443229470f14ce424dabeeb86a51
Mouhamed Lo <160361321+m3dev4@users.noreply.github.com>
2024-06-03 15:53:21 +0000

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import path from "path"
+import express  from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv"
+
+
+
+const app = express()
+
+app.use(express.json())
+app.use()
---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+ 
+import './App.css'
+
+const App = () => {
+
+  return (
+    <>
+      <h1 className="text-center">Happy to use React</h1>
+       
+    </>
+  )
+}
+
+export default App
